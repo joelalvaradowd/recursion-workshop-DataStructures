c249dd57090ad80a025c99eb5b070ca93b3b5437
Viviana Vera Falconí <33163800+vivianavf@users.noreply.github.com>
2021-06-05 10:07:45 -0500

    Corrección método 2

--- Taller1/src/Taller1.java
+++ Taller1/src/Taller1.java
@@ -1,6 +1,6 @@
 
 public class Taller1 {
-
+	
     public static Character retornarPrimeraLetra(String cadena) {
         if (cadena.isEmpty()) {
             return null;
@@ -13,40 +13,22 @@ public class Taller1 {
         }
     }
 
+// El stringBuilder trabaja sobre la misma cadena y es más eficiente
     public static String replaceOA(String cadena, int n) {
-        return replace(cadena, n, 0);
-    }
-
-    private static String replace(String cadena, int n, int inicio) {
         StringBuilder sb = new StringBuilder(cadena);
-        if (inicio > cadena.length() || n == 0) {
-            return sb.toString();
-        } else if (sb.charAt(inicio) == 'o') {
-            sb.replace(inicio, inicio + 1, "a");
-        } else if (cadena.charAt(inicio) == 'O') {
-            sb.replace(inicio, inicio + 1, "A");
-        } else {
-            return replace(sb.toString(), n, inicio + 1);
-        }
-        return replace(sb.toString(), n - 1, inicio + 1);
-
-    }
-    // Se cae con el string "Angel Guale";
-
-    public static String replaceOA2(String cadena, int n) {
         if (n == 0) {
             return cadena;
         }
-
         if (cadena.isEmpty()) {
             return "";
         } else {
-            String inicio = cadena.substring(0, 1);
-            if (inicio.equals("o")) {
-                return "a" + replaceOA2(cadena.substring(1), n - 1);
+            StringBuilder inicio = new StringBuilder(sb.substring(0, 1));            
+            if (inicio.toString().equals("o")) {                
+                return  new StringBuilder("a").append(replaceOA(cadena.substring(1), n - 1)).toString();
             } else {
-                return inicio + replaceOA2(cadena.substring(1), n);
+                return inicio.append(replaceOA(cadena.substring(1), n)).toString();
             }
         }
-    }
+    }    
 }
+    
