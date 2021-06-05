88361efa32f6f3e0f0151744335fc66a96f7cfe2
Viviana Vera Falconí <33163800+vivianavf@users.noreply.github.com>
2021-06-04 19:26:16 -0500

    Ejercicio 2

--- Taller1/src/Ejercicio1.java
+++ Taller1/src/Ejercicio1.java
@@ -1,4 +1,6 @@
+
 public class Ejercicio1 {
+	
     public static Character retornarPrimeraLetra(String cadena) {
         if (cadena.isEmpty()) {
             return null;
@@ -9,5 +11,25 @@ public class Ejercicio1 {
                 return retornarPrimeraLetra(cadena.substring(1, cadena.length()));
             }
         }
+    }
+    
+    
+    public static String replaceOA(String cadena, int n) {
+        return replace(cadena, n, 0);
+    }
+    
+    private static String replace(String cadena, int n, int inicio) {    	
+    	StringBuilder sb = new StringBuilder(cadena);     	
+    	if(inicio>cadena.length() || n==0) {    		
+    		return sb.toString();    		
+    	}else if(sb.charAt(inicio)=='o') {      		
+    		sb.replace(inicio, inicio+1, "a");   
+    	}else if(cadena.charAt(inicio)=='O') {    		
+    		sb.replace(inicio, inicio+1, "A");		    		
+    	}else {
+    		return replace(sb.toString(), n, inicio+1);
+    	}
+        return replace(sb.toString(), n-1, inicio+1); 
+    	
     }
 }
