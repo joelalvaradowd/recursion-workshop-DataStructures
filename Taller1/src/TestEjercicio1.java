7ba7a077d2b9f73f5d17a898478f5e5acf65966d
Viviana Vera Falconí <33163800+vivianavf@users.noreply.github.com>
2021-06-04 19:28:39 -0500

    Update del Test

--- Taller1/src/TestEjercicio1.java
+++ Taller1/src/TestEjercicio1.java
@@ -1,6 +1,13 @@
+
+
 public class TestEjercicio1 {
     public static void main(String[] args){
         System.out.println(Ejercicio1.retornarPrimeraLetra("yosoyESPOL"));
         System.out.println(Ejercicio1.retornarPrimeraLetra("hola"));
+        System.out.println(Ejercicio1.replaceOA("Buenos días a todos!, 2"));
+        System.out.println(Ejercicio1.replaceOA("ooooooooooooooooooo!, 5"));
+        
+        
     }
+    
 }
