1d9588a6e577d2df7dd52dbe49e34b008164242c
Joel Alvarado <joealalv@espol.edu.ec>
2021-06-05 03:13:23 -0500

    Creacion del metodo OA2

--- Taller1/src/Taller1.java
+++ Taller1/src/Taller1.java
@@ -1,6 +1,6 @@
 
 public class Taller1 {
-	
+
     public static Character retornarPrimeraLetra(String cadena) {
         if (cadena.isEmpty()) {
             return null;
@@ -12,24 +12,41 @@ public class Taller1 {
             }
         }
     }
-    
-    
+
     public static String replaceOA(String cadena, int n) {
         return replace(cadena, n, 0);
     }
-    
-    private static String replace(String cadena, int n, int inicio) {    	
-    	StringBuilder sb = new StringBuilder(cadena);     	
-    	if(inicio>cadena.length() || n==0) {    		
-    		return sb.toString();    		
-    	}else if(sb.charAt(inicio)=='o') {      		
-    		sb.replace(inicio, inicio+1, "a");   
-    	}else if(cadena.charAt(inicio)=='O') {    		
-    		sb.replace(inicio, inicio+1, "A");		    		
-    	}else {
-    		return replace(sb.toString(), n, inicio+1);
-    	}
-        return replace(sb.toString(), n-1, inicio+1); 
-    	
+
+    private static String replace(String cadena, int n, int inicio) {
+        StringBuilder sb = new StringBuilder(cadena);
+        if (inicio > cadena.length() || n == 0) {
+            return sb.toString();
+        } else if (sb.charAt(inicio) == 'o') {
+            sb.replace(inicio, inicio + 1, "a");
+        } else if (cadena.charAt(inicio) == 'O') {
+            sb.replace(inicio, inicio + 1, "A");
+        } else {
+            return replace(sb.toString(), n, inicio + 1);
+        }
+        return replace(sb.toString(), n - 1, inicio + 1);
+
+    }
+    // Se cae con el string "Angel Guale";
+
+    public static String replaceOA2(String cadena, int n) {
+        if (n == 0) {
+            return cadena;
+        }
+
+        if (cadena.isEmpty()) {
+            return "";
+        } else {
+            String inicio = cadena.substring(0, 1);
+            if (inicio.equals("o")) {
+                return "a" + replaceOA2(cadena.substring(1), n - 1);
+            } else {
+                return inicio + replaceOA2(cadena.substring(1), n);
+            }
+        }
     }
 }
--- Taller1/src/Test.java
+++ Taller1/src/Test.java
@@ -1,13 +1,11 @@
 
-
 public class Test {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println(Taller1.retornarPrimeraLetra("yosoyESPOL"));
         System.out.println(Taller1.retornarPrimeraLetra("hola"));
-        System.out.println(Taller1.replaceOA("Buenos días a todos!, 2"));
-        System.out.println(Taller1.replaceOA("ooooooooooooooooooo!, 5"));
-        
-        
+        System.out.println(Taller1.replaceOA2("Buenos días a todos!", 2));
+        System.out.println(Taller1.replaceOA2("Angel Guale!", 5));
+
     }
-    
+
 }
