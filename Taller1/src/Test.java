a77337f39180d272642c91ac11c9a1db47f22fca
Viviana Vera Falconí <33163800+vivianavf@users.noreply.github.com>
2021-06-04 19:34:53 -0500

    Rename

---
+++ Taller1/src/Test.java
@@ -0,0 +1,13 @@
+
+
+public class Test {
+    public static void main(String[] args){
+        System.out.println(Taller1.retornarPrimeraLetra("yosoyESPOL"));
+        System.out.println(Taller1.retornarPrimeraLetra("hola"));
+        System.out.println(Taller1.replaceOA("Buenos días a todos!, 2"));
+        System.out.println(Taller1.replaceOA("ooooooooooooooooooo!, 5"));
+        
+        
+    }
+    
+}
