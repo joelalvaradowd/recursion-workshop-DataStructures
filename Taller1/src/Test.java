d7faf5ef86840a68fae23d9154f8e01d669086ed
Viviana Vera Falconí <33163800+vivianavf@users.noreply.github.com>
2021-06-05 10:08:51 -0500

    Prueba de replaceOA con varias cadenas

--- Taller1/src/Test.java
+++ Taller1/src/Test.java
@@ -1,11 +1,13 @@
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println(Taller1.retornarPrimeraLetra("yosoyESPOL"));
-        System.out.println(Taller1.retornarPrimeraLetra("hola"));
-        System.out.println(Taller1.replaceOA2("Buenos días a todos!", 2));
-        System.out.println(Taller1.replaceOA2("Angel Guale!", 5));
-
+        System.out.println(Ejercicio1.retornarPrimeraLetra("yosoyESPOL"));
+        System.out.println(Ejercicio1.retornarPrimeraLetra("hola"));                
+       
+        System.out.println(Ejercicio1.replaceOA("Esta es una cadena sin el caracter", 3));
+        System.out.println(Ejercicio1.replaceOA("Buenos dias a todos!", 5));
+        System.out.println(Ejercicio1.replaceOA("ee", 5));
+        System.out.println(Ejercicio1.replaceOA("Buenos dias a todos!", 2));
     }
 
 }
