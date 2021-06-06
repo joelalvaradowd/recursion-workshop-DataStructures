58a84ab8ae1f0b02ef1821e2ded92292f2ec8a8c
Joel Alvarado <joealalv@espol.edu.ec>
2021-06-06 14:34:05 -0500

    Proyecto probado en ambiente de produccion

--- Taller1/src/Test.java
+++ Taller1/src/Test.java
@@ -1,4 +1,4 @@
-import jdk.javadoc.internal.doclets.formats.html.SourceToHTMLConverter;
+//import jdk.javadoc.internal.doclets.formats.html.SourceToHTMLConverter;
 
 public class Test {
     public static void main(String[] args) {
@@ -7,13 +7,15 @@ public class Test {
         System.out.println(Taller1.retornarPrimeraLetra("hola"));
         System.out.println(Taller1.retornarPrimeraLetra("nPoe"));
         System.out.println(Taller1.retornarPrimeraLetra("Sin mayuscula")); 
-        System.out.println(Taller1.retornarPrimeraLetra("uzuM"));                     
+        System.out.println(Taller1.retornarPrimeraLetra("uzuM"));
+        System.out.println(Taller1.retornarPrimeraLetra(""));                     
         
         System.out.println("Reemplazar o con a\n");
         System.out.println(Taller1.replaceOA("Esta es una cadena sin el caracter", 3));
         System.out.println(Taller1.replaceOA("Buenos dias a todos!", 5));
         System.out.println(Taller1.replaceOA("ee", 5));
         System.out.println(Taller1.replaceOA("Buenos dias a todos!", 2));
+        System.out.println(Taller1.replaceOA("Angel Guale", 3));
     }
 
 }
